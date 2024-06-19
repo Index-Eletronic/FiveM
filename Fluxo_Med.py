fc2dcbbd0883b5411e79fb537a66cc137f68426a
Fil Tuon <54403070+Index-Eletronic@users.noreply.github.com>
2024-06-19 17:07:53 -0300

    .
    .

--- Fluxo_Med.py
+++ Fluxo_Med.py
@@ -3,20 +3,68 @@ from time import sleep
 
 import pyautogui
 
+#unbinds = [0,1,2,3,'f9']
+# #binds =
+
+
+'''
+1 cobrança
+digitar 1
+f8 > cobrar id 6000
+enter
+f8
+y 2 x
+esperar confirmar, y ou n
+caso seja n, repetir o processo 2 x
+
+
+'''
+
+
 id_user = 0
 
 
 def reviver():
     global id_user
+    tentativa = 0
 
     sleep(2)
     #PEgar o id
-
     pyautogui.press('1')
+    sleep(2)
+    pyautogui.press('1') #f8
+    sleep(1)
+    pyautogui.write(f'cobrar {id_user} 6000')
+    sleep(3)
+    pyautogui.press ('1') #enter
+    sleep(1)
+    pyautogui.press('1') #f8
+    sleep(1)
+
+    escolha = str(input("Paciente aceitou? [S] | [N]".upper().strip()))
+    if escolha == "S" or escolha == "s":
+        tratamento()
+    if tentativa == 2:
+        tentativa +=1
+        reviver()
+    else:
+        opcoes()
+
 
 
 def tratamento ():
     sleep(2)
+    #PEgar o id
+    pyautogui.press('1')
+    sleep(2)
+    pyautogui.press('1') #f8
+    sleep(1)
+    pyautogui.write(f'cobrar {id_user} 6000')
+    sleep(3)
+    pyautogui.press ('1') #enter
+    sleep(1)
+    pyautogui.press('1') #f8
+    sleep(5)
 
 def opcoes():
     global id_user
@@ -29,11 +77,13 @@ def opcoes():
     if choice == 1:
         print("Digite o ID do paciente: ")
         id_user = int(input())
-        reviver
+        reviver()
         print("Paciente vai querer tratamento:")
-        choice2 = int(input(''),UPPER)
-        if choice == "S":
-            
+        choice2 = str(input('').upper().strip())
+        if choice2 == "S":
+            tratamento()
+        else:
+            opcoes()
 
     elif choice == 2:
         tratamento
